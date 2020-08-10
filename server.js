acf860b14b31877b415e65ffed24e61ee8e3fbf3
Allan <allanl_95@hotmail.com>
2020-08-10 12:23:21 -0700

    CRUD implementations

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const exphbs = require('express-handlebars'); // Handlebars
 const path = require('path');
 const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
@@ -25,6 +26,15 @@ process.env.NODE_ENV === 'development' ? app.use(morgan('dev')) : ''
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method overrride
+app.use(methodOverride((req, res) => {
+	if(req.body && typeof req.body === 'object' && '_method' in req.body) {
+		let method = req.body._method
+		delete req.body._method
+		return method
+	}
+}))
+
 //Sessions ****
 app.use(session({
 	secret: 'keyboard cat',
@@ -36,11 +46,17 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set Global variable
+app.use((req, res, next) => {
+	res.locals.user = req.user || null
+	next()
+})
+
 
 //Handlebars Helpers
-const { formatDate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
 // Handlebars
-app.engine('.hbs', exphbs({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({ helpers: { formatDate, stripTags, truncate, editIcon, select }, defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
